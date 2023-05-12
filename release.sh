fe8e7f8a287f3edce7a7136b426f12334e282343
Ronald Casili <nvlled@gmail.com>
2023-05-12 19:16:59 +0800

    Fix release build

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd react-src
+npm run build
+cd ..
+neu build -r --copy-storage
+cat <<EOF > ./dist/catchsup/catchsup.sh
+#!/bin/bash
+cd "\$(dirname "\$0")"
+./catchsup-linux_x64
+EOF
+chmod +x ./dist/catchsup/catchsup.sh
